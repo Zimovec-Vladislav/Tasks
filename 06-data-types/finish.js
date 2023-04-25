885ad0a025d3c658211d3e71e49250b29827e476
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-04-25 20:47:52 +0300

    Task -> Data types

---
+++ 06-data-types/finish.js
@@ -0,0 +1,15 @@
+const string = 'строка'
+const number = 47
+const bool = true
+const null1 = null
+let undefined
+const object = { myName: 'Vladislav'}
+const massive = []
+
+console.log(typeof string)
+console.log(typeof number)
+console.log(typeof bool)
+console.log(typeof null1)
+console.log(typeof undefined1)
+console.log(typeof object)
+console.log(typeof massive)
