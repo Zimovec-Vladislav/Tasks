428fdd3f1c53dc17e8815161a5e16b47f6e0e8ad
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-04-25 20:22:47 +0300

    Task -> Strings Concatenation

---
+++ 04-strings-concatenation/finish.js
@@ -0,0 +1,6 @@
+const myFirstName = 'Vladislav'
+const myLastName = 'Zimovec'
+const myProfession = 'Programmer'
+
+const sumString = "My name is " + myFirstName + ' ' + myLastName + " and I " + myProfession
+console.log(sumString)
