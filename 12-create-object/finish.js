269fea7004c3629f62f6f5ddc735d7b1805fe8a9
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-05-08 20:47:17 +0300

    lesson 12 create object

---
+++ 12-create-object/finish.js
@@ -0,0 +1,7 @@
+const myObject = {
+    name: 'Vladislav',
+    surname: 'Zimovec',
+    favoriteNumber: 777
+}
+
+console.log(`My name is ${myObject.name} ${myObject.surname} and my favorite number is ${myObject.favoriteNumber}`)
