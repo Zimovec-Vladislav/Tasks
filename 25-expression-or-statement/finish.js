0ca1af7472095db6006336a2492e865c74452cc4
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-06-07 19:28:07 +0300

    25-expression-or-statement

---
+++ 25-expression-or-statement/finish.js
@@ -0,0 +1,20 @@
+15 //expression statement
+
+const myObject = { //statement
+  x: 10,
+  y: true,
+}
+
+myObject.z = 'abc' //expression statement
+
+delete myObject.x //expression statement
+
+let newVariable //statement
+
+newVariable = 30 + 5 //expression statement
+
+console.log(newVariable) //expression statement
+
+if (newVariable > 10) { //statement
+  console.log(`${newVariable} больше 10`) //expression statement
+}
