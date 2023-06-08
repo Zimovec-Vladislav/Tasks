2a542afa9f4d2a84e5fb188c93c3d62014c74a84
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-06-08 20:20:51 +0300

    34-array-of-objects

---
+++ 34-array-of-objects/finish.js
@@ -0,0 +1,9 @@
+const carsArray = [
+    {carBrand: 'Mercedez', price: 100000, isAvailableForSale: true},
+    {carBrand: 'Ford', price: 50000, isAvailableForSale: false},
+    {carBrand: 'Nissan', price: 60000, isAvailableForSale: true}
+]
+
+carsArray.push({carBrand: 'Dodge', price: 150000, isAvailableForSale: true})
+
+console.log(carsArray)
