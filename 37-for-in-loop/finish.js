aaa40cb5e57ab68214b5ee9c576e4639fb0a66d6
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-06-10 17:17:11 +0300

    37-for-in-loop

---
+++ 37-for-in-loop/finish.js
@@ -0,0 +1,13 @@
+const myObject = {
+  name: 'Mike',
+  age: 30,
+  city: 'London',
+}
+
+Object.prototype.country = 'England'
+
+for (let key in myObject) {
+  if(myObject.hasOwnProperty(key)) {
+  console.log(myObject[key])
+  }
+}
