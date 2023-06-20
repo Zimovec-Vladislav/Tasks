2f2cfb77861b6028a6ecaa7dda9fd6e0c7b3af1e
Vladislav <boss.zimovet@gmail.com>
2023-06-20 19:34:06 +0300

    45-includes-object-or-array

---
+++ 45-includes-object-or-array/finish.js
@@ -0,0 +1,32 @@
+const isElementInArray = (searchElement, inputArray) => {
+  if (typeof searchElement === 'object') {
+    return inputArray
+      .map((el) => JSON.stringify(el))
+      .includes(JSON.stringify(searchElement))
+  }
+  return inputArray.includes(searchElement)
+}
+
+const tags = [
+  ['javascript', 'es6'],
+  ['css', 'flexbox'],
+  ['html', 'web-browser'],
+]
+
+const fruits = [
+  { title: 'Orange', quantity: 10 },
+  { title: 'Banana', quantity: 5 },
+  { title: 'Apple', quantity: 25 },
+]
+
+const primitiveTypesArray = [25, 'x', true, undefined, null]
+
+console.log(isElementInArray(['css', 'flexbox'], tags)) // true
+
+console.log(isElementInArray(['flexbox', 'css'], tags)) // false
+
+console.log(isElementInArray({ title: 'Apple', quantity: 25 }, fruits)) // true
+
+console.log(isElementInArray({ title: 'Banana' }, fruits)) // false
+
+console.log(isElementInArray(25, primitiveTypesArray)) // true
