2ed59341b8b472e4bb830a2fe24ef6699a7211d5
Vladislav <boss.zimovet@gmail.com>
2023-06-21 21:37:22 +0300

    47-reduce-array-of-objects

---
+++ 47-reduce-array-of-objects/finish.js
@@ -0,0 +1,33 @@
+const popularPostsIds = (posts, minimalComentsQty) => {
+  return posts.reduce(
+    (postsIds, post) => 
+      post.comments >= minimalComentsQty
+      ? postsIds.concat([post.postId])
+      : postsIds,
+      []
+  )
+}
+
+const inputPosts = [
+  {
+    title: 'Как быстро выучить JavaScript?',
+    postId: 3421,
+    comments: 25,
+  },
+  {
+    title: 'Где используется JavaScript?',
+    postId: 5216,
+    comments: 3,
+  },
+  {
+    title: 'Какая разница между React и Angular?',
+    postId: 8135,
+    comments: 12,
+  },
+]
+
+console.log(popularPostsIds(inputPosts, 10)) // [3421, 8135]
+
+console.log(popularPostsIds(inputPosts, 15)) // [3421]
+
+console.log(popularPostsIds(inputPosts, 50)) // []
